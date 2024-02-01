783805981111bfef5bb04871d90c44ee461d5f3b
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-01 16:23:28 -0500

    complete

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce((a, e) => {
+    return a + e;
+})
+
+const dice = [1,3,5,2,6,2];
+
+let tally = dice.reduce((a, e) => {
+    
+    if (e in a) {
+        a[e]++;
+    } else {
+        a[e] = 1;
+    }
+    return a;
+}, {})
